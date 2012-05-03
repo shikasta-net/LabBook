e30c027e22b83e8e79c56569c58a393dd3abc9e2
Kym Eden <shikasta.net@gmail.com>
2012-05-03 15:47:08 +0100

    restructured the database for generic content

--- controllers/default.py
+++ controllers/default.py
@@ -4,10 +4,9 @@ def index():
     
 def edit():
     this_page = db.page(request.args(0)) or redirect(URL('index'))
-    db.text_box.page_id.default = this_page.id
-    form = crud.create(db.text_box)
-    t_boxes = db(db.text_box.page_id==this_page.id).select()
-    return dict(page=this_page, form=form, boxes=t_boxes)
+    db.container_box.page_id.default = this_page.id
+    t_boxes = db(db.container_box.page_id==this_page.id).select()
+    return dict(page=this_page, boxes=t_boxes)
     
 def call():
     session.forget()
@@ -17,7 +16,7 @@ def call():
 def move_box(id,x,y):
     rcode = 0
     try :
-        db(db.text_box.id==id).update(position_x=x, position_y=y)       
+        db(db.container_box.id==id).update(position_x=x, position_y=y, modified_on=request.now)       
     except Exception, e :
         print 'oops: %s' % e
         response.headers['Status'] = '400'
@@ -32,7 +31,7 @@ def move_box(id,x,y):
 def resize_box(id,w,h):
     rcode = 0
     try :
-        db(db.text_box.id==id).update(width=w, height=h)
+        db(db.container_box.id==id).update(width=w, height=h, modified_on=request.now)
     except Exception, e :
         print 'oops: %s' % e
         response.headers['Status'] = '400'
@@ -47,7 +46,7 @@ def resize_box(id,w,h):
 def new_box(page_id):
     rcode = 0
     try :
-        new_id = db.text_box.insert(body='some test text for a new record', page_id=page_id)
+        new_id = db.container_box.insert(body='some test text for a new record', page_id=page_id)
     except Exception, e :
         print 'oops: %s' % e
         response.headers['Status'] = '400'
@@ -62,7 +61,9 @@ def new_box(page_id):
 def del_box(box_id):
     rcode = 0
     try :
-        db(db.text_box.id==box_id).delete()
+        contents = db(db.content.id==db(db.container_box.id==box_id).content_id).select()
+        #if exists content, delete it.
+        db(db.container_box.id==box_id).delete()
     except Exception, e :
         print 'oops: %s' % e
         response.headers['Status'] = '400'
