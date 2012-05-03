234ccfe00e112910d53a10aa2d459bb9daeb2786
Kym Eden <shikasta.net@gmail.com>
2012-05-03 17:43:17 +0100

    loads filename as file content and uses content type as part of the ID

--- controllers/default.py
+++ controllers/default.py
@@ -5,12 +5,22 @@ def index():
 def edit():
     this_page = db.page(request.args(0)) or redirect(URL('index'))
     db.container_box.page_id.default = this_page.id
-    t_boxes = db(db.container_box.page_id==this_page.id).select()
-    return dict(page=this_page, boxes=t_boxes)
+    boxes = db(db.container_box.page_id==this_page.id).select()
+    contents = {}
+    for box in boxes :
+        if box.content_id is None :
+            contents[box.id] = {'file_type':'text', 'file_content':'blank text box'}
+        else:
+            contents[box.id] = get_contents(box.content_id)
+    return dict(page=this_page, boxes=boxes, contents=contents)
     
 def call():
     session.forget()
     return service()
+    
+def get_contents(content_id):
+    content = db(db.content.id==content_id).select().first()
+    return dict(file_type=content.file_type[0], file_content=content.file_name )
     
 @service.run
 def move_box(id,x,y):
@@ -46,7 +56,7 @@ def resize_box(id,w,h):
 def new_box(page_id):
     rcode = 0
     try :
-        new_id = db.container_box.insert(body='some test text for a new record', page_id=page_id)
+        new_id = db.container_box.insert(page_id=page_id)
     except Exception, e :
         print 'oops: %s' % e
         response.headers['Status'] = '400'
