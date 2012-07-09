feeae417e71cbb492f8a96ce97691c95a510c690
Kym Eden <shikasta.net@gmail.com>
2012-07-09 17:15:10 +0100

    Replaced text editor

--- static/js/texteditor.js
+++ static/js/texteditor.js
@@ -6,8 +6,144 @@ var DOLLAR = 36;
 var CTRLDOLLAR = 52;
 
 function initialiseEditor() {
-	$('body').append("<div id='textEditorContainer'><div id='textEditor'></div></div>");
-	textEd = new tinyMCE.Editor('textEditor', {
+	$('body').append("<div class='claro' id='textEditorContainer'><div id='textEditor'></div></div>");
+	require(["dijit/Editor",
+		"dojo/parser",
+		"dojo/domReady!",
+		"dijit/_editor/plugins/FontChoice",
+		"dijit/_editor/plugins/LinkDialog",
+		"dojox/editor/plugins/TextColor"],
+		function(Editor){
+			textEd = new Editor({ plugins: ["bold","italic","|","cut","copy","paste","|","insertUnorderedList"],
+				extraPlugins : ["fontName","fontSize","formatBlock","foreColor","hiliteColor","|","createLink"]
+			}, "textEditor");
+
+			textEd.addStyleSheet("../../css/page_layout.css");
+
+			textEd.connect(textEd, "onKeyPress", function(e){
+				// If we're currently editing an equation...
+				//textEd.window probably only works outside IE
+				caretNode = textEd.window.getSelection().anchorNode.parentNode;
+				if ($(caretNode).hasClass('eqn')) {
+					// Pressed $ in inline eqn? Boost eqn to display
+					if (e.charCode == DOLLAR && $(caretNode).hasClass('inline')) {
+						e.preventDefault();
+						e.stopPropagation();
+						$(caretNode).removeClass('inline');
+						$(caretNode).addClass('display');
+					}
+					// Pressed Ctrl-$ in inline? Remove eqn and leave plaintext
+					if (e.charCode == CTRLDOLLAR && $(caretNode).hasClass('inline')) {
+						e.preventDefault();
+						e.stopPropagation();
+						var temp = $(caretNode).html();
+						$(caretNode).remove();
+						textEd.execCommand('inserthtml', temp);
+						//textEd.window.getSelection().deleteFromDocument();
+						//textEd.window.getSelection().anchorNode.parentNode.innerHTML(temp);
+					}
+					// Pressed Ctrl-$ in display eqn? Reduce to inline eqn
+					if (e.charCode == CTRLDOLLAR && $(caretNode).hasClass('display')) {
+						e.preventDefault();
+						e.stopPropagation();
+						$(caretNode).removeClass('display');
+						$(caretNode).addClass('inline');
+					}
+
+				} else {
+					// $ pressed outside equation? Make new eqn.
+					if (e.charCode == DOLLAR) {
+						e.preventDefault();
+						e.stopPropagation();
+						var eqnIDCounter = 0;
+						while ($('#eqn_' + eqnIDCounter).get(0)) { eqnIDCounter++; }
+						var newEqn = textEd.document.createElement('span');
+						$(newEqn).attr('id', 'eqn_' + eqnIDCounter);
+						$(newEqn).addClass('eqn inline');
+						console.log($('<p></p>').append(newEqn).html());
+						var convert = !textEd.window.getSelection().isCollapsed;
+						if (convert) {
+							textEd.window.getSelection().getRangeAt(0).surroundContents(newEqn);
+						} else {
+							var newnode = textEd.document.createTextNode(" ");
+							newEqn.appendChild(newnode);
+						}
+						textEd.execCommand("inserthtml", "&nbsp;" + $('<p></p>').append(newEqn).html() + "&nbsp;");
+						var rng = textEd.document.createRange();
+						var eqnID = textEd.document.getElementById("eqn_" + eqnIDCounter);
+						if (convert) {
+							rng.selectNodeContents(eqnID);
+							rng.collapse(false);
+						} else {
+							rng.setStart(eqnID.childNodes[0],0);
+							rng.setEnd(eqnID.childNodes[0],1);
+						}
+						var sel = textEd.window.getSelection();
+						sel.removeAllRanges();
+						sel.addRange(rng);
+						// ed.selection.setNode(newEqn);
+						// ed.selection.select(ed.dom.select('#'+newEqnID)[0].firstChild);
+					}
+				}
+
+
+			});
+			// Update content box every time user presses a key.
+			// TODO: Less intensive update scheme? (eg track caret position and update only updated elements?) Complicated!
+			textEd.connect(textEd, "onKeyUp", function(e){
+
+/*				if (e.keyCode == KEYLEFT
+					&& ed.selection.isCollapsed()
+					&& ed.selection.getRng(true).startOffset == 0
+					&& ed.selection.getRng(true).startContainer.parentElement.nodeName == 'SPAN') {
+						e.preventDefault();
+						e.stopPropagation();
+						console.log('cursor leaving SPAN keyup');
+				}*
+*/
+				loadEditorToBox(edCurrentTarget);
+			});
+			textEd.connect(textEd, "onKeyDown", function(e){
+/*
+				if (e.keyCode == KEYLEFT
+					&& ed.selection.isCollapsed()
+					&& ed.selection.getRng(true).startOffset == 1
+					&& ed.selection.getRng(true).startContainer.parentElement.nodeName == 'SPAN') {
+						e.preventDefault();
+						e.stopPropagation();
+						console.log('cursor left leaving SPAN keydown');
+						//var rng = ed.selection.getRng(true);
+						//rng.setStart(rng.startContainer, 0);
+						//rng.collapse(true);
+				} else if (e.keyCode == KEYRIGHT
+					&& ed.selection.isCollapsed()
+					&& ed.selection.getRng(true).startOffset == ed.selection.getRng(true).startContainer.nodeValue.length
+					&& ed.selection.getRng(true).startContainer.parentElement.nodeName == 'SPAN') {
+						e.preventDefault();
+						e.stopPropagation();
+						console.log('cursor right leaving SPAN keydown');
+				}*/
+
+				// Prevent carriage return in box. Dojo gets overactive with the <br /> if you don't.
+				if ($(textEd.window.getSelection().anchorNode.parentNode).hasClass('eqn')) {
+
+					switch (e.keyCode) {
+						case 13:
+							// Prevent enter key in equations
+							e.preventDefault();
+							e.stopPropagation();
+							break;
+					}
+				}
+
+			});
+		}
+	);
+	hideEditor();
+
+
+
+	/*textEd = new tinyMCE.Editor('textEditor', {
 			mode : "none",
 			theme : "simple",
 			skin: "default",
@@ -25,7 +161,7 @@ function initialiseEditor() {
 						e.preventDefault();
 						e.stopPropagation();
 						console.log('cursor leaving SPAN keypress');
-				}*/
+				}*
 				// If we're currently editing an equation...
 				if (ed.dom.hasClass(ed.selection.getNode(), 'eqn')) {
 					// Pressed $ in inline eqn? Boost eqn to display
@@ -78,7 +214,7 @@ function initialiseEditor() {
 					e.preventDefault();
 					e.stopPropagation();
 					console.log('cursor leaving SPAN keyup');
-			}*/
+			}*
 
 			loadEditorToBox(edCurrentTarget);
 		});
@@ -102,7 +238,7 @@ function initialiseEditor() {
 					e.preventDefault();
 					e.stopPropagation();
 					console.log('cursor right leaving SPAN keydown');
-			}*/
+			}*
 
 			// Prevent carriage return in box. tinyMCE gets overactive with the <br /> if you don't.
 			if (ed.dom.hasClass(ed.selection.getNode(), 'eqn')) {
@@ -118,8 +254,7 @@ function initialiseEditor() {
 		});
 			}
 	});
-	textEd.render();
-	hideEditor();
+	*/
 }
 
 // Content box to editor conversion
@@ -130,33 +265,33 @@ function loadBoxToEditor(target) {
 	tbox_contents.find('.MathJax_Display').remove();
 	tbox_contents.find('.MathJax').remove();
 	tbox_contents.find('script').replaceWith(function() {
-	var newSpan = $('<span></span>');
-	newSpan.attr('id', $(this).attr('id'));
-		newSpan.addClass('eqn');
-	if ($(this).attr('type') == "math/tex; mode=display") {
-		newSpan.addClass('display');
-	} else if ($(this).attr('type') == "math/tex; mode=inline") {
-		newSpan.addClass('inline');
-	} else {
-		throw "Mangled MathJax type attribute: " + $(this).attr('type');
-	}
-	newSpan.html($(this).text());
-	return newSpan;
+		var newSpan = $('<span></span>');
+		newSpan.attr('id', $(this).attr('id'));
+			newSpan.addClass('eqn');
+		if ($(this).attr('type') == "math/tex; mode=display") {
+			newSpan.addClass('display');
+		} else if ($(this).attr('type') == "math/tex; mode=inline") {
+			newSpan.addClass('inline');
+		} else {
+			throw "Mangled MathJax type attribute: " + $(this).attr('type');
+		}
+		newSpan.html($(this).text());
+		return newSpan;
 	});
-	textEd.setContent(tbox_contents.html());
-
+	textEd.set("value",tbox_contents.html());
+	textEd.focus();
 }
 
 // Copies editor contents to specified box
 // Reverse loadBoxToEditor: replace spans with scripts (decoding html entities) and re-typeset.
 function loadEditorToBox(target) {
 
-	editorContents = $('<div />').html($(textEd.getContent()).clone());
+	editorContents = $('<div />').html($(textEd.get("value")).clone());
 	editorContents.find('span.eqn').replaceWith(function() {
 		var display = $(this).hasClass('display') ? 'display' : 'inline';
 		var newScript = $('<script type="math/tex; mode=' + display + '"></script>');
 		newScript.attr('id', $(this).attr('id'));
-		newScript.text($(this).html());
+		newScript.text($(this).text());
 		return newScript;
 	});
 	target.children(".textbox").html(editorContents.html());
@@ -165,27 +300,23 @@ function loadEditorToBox(target) {
 
 function showEditor(target) {
 	edCurrentTarget = target;
-	textEd.setContent('<p></p>');
+	textEd.set("value","<p></p>");
 	$('#textEditorContainer').slideDown('1000');
-	textEd.setProgressState(1);
+	//~ textEd.setProgressState(1);
 	if (target.hasClass('empty')) {
 		console.log("Text editor called on something empty.");
 		console.log(target);
-		target.find('#option_bar').after("<div class='textbox' id='"+target.attr('id').replace('c','txt')+"'></div>");
- 		//the p is needed to force mce to be the size of the container, sadly it removes it if you dont put anything in it first time
+		target.find('#option_bar').after("<div class='textbox' id='"+target.attr('id').replace('c','txt')+"'><p></p></div>");
 		target.removeClass("empty");
 		loadBoxToEditor(target);
-		textEd.setProgressState(0);
-		textEd.focus();
 	} else {
 		loadBoxToEditor(target);
-		textEd.setProgressState(0);
-		textEd.focus();
 	} /*else {
 		console.log("Text editor called on something which was neither a textbox nor empty.");
 		textEd.setProgressState(0);
 		hideEditor();
 	}*/
+	//~ textEd.setProgressState(0);
 }
 
 function hideEditor() {
@@ -199,7 +330,7 @@ function saveEditor() {
 	return;
 	}
 	loadEditorToBox(edCurrentTarget);
-	textEd.setProgressState(1);
+	//~ textEd.setProgressState(1);
 	if (window.BlobBuilder) {
 		var bb = new BlobBuilder();
 	} else if (window.MozBlobBuilder) {
@@ -219,5 +350,5 @@ function saveEditor() {
 	bb.append(boxContent.html());
 	var blob = bb.getBlob('text/html');
 	content.handleSaveContent(pageID, edCurrentTarget.attr("id").replace('c',''), blob, { name:'textbox'+edCurrentTarget.attr("id").replace('c','')+'.html', type:'text/html', size: blob.size });
-	textEd.setProgressState(0);
+	//~ textEd.setProgressState(0);
 }
